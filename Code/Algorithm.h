c89011d97ec934762517ff92797317dd14a47de3
BuildTools <unconfigured@null.spigotmc.org>
2020-09-18 13:25:36 -0500

    Added Algorithm Class

---
+++ Code/Algorithm.h
@@ -0,0 +1,31 @@
+#ifndef ALGORITHM_H
+#define ALGORITHM_H
+
+#include <string>
+using namespace std;
+
+class Algorithm
+{
+    public:
+    	//Creates an enum with every type of search
+        enum SearchingAlgo{
+            BFS_ITERATIVE = 0,
+            BFS_RECURSIVE,
+            DFS_ITERATIVE,
+            DFS_RECURSIVE,
+            DIJKSTRA,
+            A,
+            END
+        };
+    	
+    	//Defines each function that will be overloaded in the Search class
+        virtual void load(string, string, string) = 0;
+        virtual void execute() = 0;
+        virtual void display() = 0;
+        virtual void stats(string) = 0;
+        virtual void select(int) = 0;
+        virtual void save(string) = 0;
+        virtual void configure() = 0;
+		virtual void setSearch(int, int) = 0;
+};
+#endif
